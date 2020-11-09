84536ce4ca9f2a5b99040d08b0b7bf6c62fca201
Jung <bakkim05@gmail.com>
2020-11-09 14:35:31 -0600

    modificaciones a la esctructura de adquision de datos del csv

--- Nodo.py
+++ Nodo.py
@@ -8,39 +8,3 @@ class Nodo:
         self.down = None
         self.left = None
         self.right = None
-
-    # def headGet(self):
-    #     return self.head
-    # def headSet(self,new):
-    #     self.head = new
-    #     return
-
-    # def colorGet(self):
-    #     return self.color
-    # def colorSet(self,new):
-    #     self.color = new
-    #     return
-
-    # def upGet(self):
-    #     return self.up
-    # def upSet(self,new):
-    #     self.up = new
-    #     return
-
-    # def downGet(self):
-    #     return self.down
-    # def downSet(self,new):
-    #     self.down = new
-    #     return
-
-    # def leftGet(self):
-    #     return self.left
-    # def leftSet(self,new):
-    #     self.left = new
-    #     return
-
-    # def rightGet(self):
-    #     return self.right
-    # def rightSet(self,new):
-    #     self.up = right
-    #     return
---
+++ Reader.py
@@ -0,0 +1,24 @@
+import csv
+
+class MatrixReader:
+    def __init__(self, path):
+        self.path = path
+        self.m = []
+    
+    
+    def leerMat(self):
+        data_array = []
+        with open(self.path, newline='') as myFile:
+            reader = csv.reader(myFile)
+            for row in reader:
+                result = row[0].split(";")
+                data = result[0]
+                data_array.append(data)
+        data_array = data_array[0:]
+
+        #Converts data to int
+        for row in data_array:
+            self.m.append(int(row))
+
+    def getMatrix(self):
+        return self.m
